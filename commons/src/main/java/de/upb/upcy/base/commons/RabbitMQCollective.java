d30715d560521310b5e73859349de7a18c776398
Andreas Dann <adann@mail.uni-paderborn.de>
2022-08-31 18:19:46 +0200

    code clean up

--- commons/src/main/java/de/upb/upcy/base/commons/RabbitMQCollective.java
+++ commons/src/main/java/de/upb/upcy/base/commons/RabbitMQCollective.java
@@ -1,7 +1,6 @@
 package de.upb.upcy.base.commons;
 
-import com.rabbitmq.client.AMQP.BasicProperties;
-import com.rabbitmq.client.AMQP.BasicProperties.Builder;
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -10,26 +9,73 @@ import com.rabbitmq.client.DeliverCallback;
 import com.rabbitmq.client.Delivery;
 import com.rabbitmq.client.Envelope;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/** Superclass to bootstrap rabbitmq collectives */
 public abstract class RabbitMQCollective {
-  private static final Logger logger = LoggerFactory.getLogger(RabbitMQCollective.class);
   public static final String DEFAULT_RABBITMQ_REPLY_TO = "amq.rabbitmq.reply-to";
   public static final int PREFETCH_COUNT = 1;
+  private static final Logger logger = LoggerFactory.getLogger(RabbitMQCollective.class);
   private final String queueName;
   private final String replyQueue;
   private final int queue_length;
+  private final String rabbitmqUser;
+  private final String rabbitmqPass;
   private final boolean workerNode;
   private final String rabbitmqHost;
+
+  /**
+   * This queue is used to reduce back pressure to the workers. It will only allow a fixed amount of
+   * messages queued with rabbit and block the enqueueing thread until there is place in the queue
+   * again.
+   */
   private ArrayBlockingQueue<Object> actor_queue;
+
   private Channel activeChannel;
+
+  /**
+   * Uses environment variables to infer field values
+   *
+   * @param queueName
+   */
+  public RabbitMQCollective(String queueName) {
+    this(
+        queueName,
+        getRabbitMQHostFromEnvironment(),
+        getRabbitMQUserFromEnvironment(),
+        getRabbitMQPassFromEnvironment(),
+        getWorkerNodeFromEnvironment(),
+        DEFAULT_RABBITMQ_REPLY_TO,
+        getActorLimit());
+  }
+
+  public RabbitMQCollective(
+      String queueName,
+      String rabbitmqHost,
+      String rabbitmqUser,
+      String rabbitmqPass,
+      boolean workerNode,
+      String replyQueueName,
+      int queue_length) {
+    this.rabbitmqUser = rabbitmqUser;
+    this.rabbitmqPass = rabbitmqPass;
+    this.workerNode = workerNode;
+    this.rabbitmqHost = rabbitmqHost;
+    this.queueName = queueName;
+    this.replyQueue = replyQueueName;
+    this.queue_length = queue_length;
+
+    logger.info("rabbitmqHost: {}", rabbitmqHost);
+    logger.info("rabbitmqUser: {}", rabbitmqUser);
+    logger.info("workerNode: {}", workerNode);
+    logger.info("ACTOR_LIMIT: {}", queue_length);
+  }
 
   public static String getRabbitMQHostFromEnvironment() {
     String res = System.getenv("RABBITMQ_HOST");
@@ -39,139 +85,198 @@ public abstract class RabbitMQCollective {
     return res;
   }
 
+  public static String getRabbitMQUserFromEnvironment() {
+    String res = System.getenv("RABBITMQ_USER");
+    if (res == null || res.isEmpty()) {
+      res = "guest";
+    }
+    return res;
+  }
+
+  public static String getRabbitMQPassFromEnvironment() {
+    String res = System.getenv("RABBITMQ_PASS");
+    if (res == null || res.isEmpty()) {
+      res = "guest";
+    }
+    return res;
+  }
+
   public static boolean getWorkerNodeFromEnvironment() {
     String res = System.getenv("WORKER_NODE");
+
     boolean workerNode;
-    if (res != null && !res.isEmpty()) {
-      workerNode = Boolean.parseBoolean(res);
-    } else {
+    if (res == null || res.isEmpty()) {
       workerNode = true;
+    } else {
+      workerNode = Boolean.parseBoolean(res);
     }
+
     return workerNode;
   }
 
   public static int getActorLimit() {
     String res = System.getenv("ACTOR_LIMIT");
     int actorLimit;
-    if (res != null && !res.isEmpty()) {
-      actorLimit = Integer.parseInt(res);
-    } else {
+    if (res == null || res.isEmpty()) {
       actorLimit = 20;
+    } else {
+      actorLimit = Integer.parseInt(res);
     }
     return actorLimit;
   }
 
-  public RabbitMQCollective(String queueName) {
-    this(
-        queueName,
-        getRabbitMQHostFromEnvironment(),
-        getWorkerNodeFromEnvironment(),
-        "amq.rabbitmq.reply-to",
-        getActorLimit());
+  private static boolean reachable() {
+    try (Socket ignored = new Socket(getRabbitMQHostFromEnvironment(), 5672)) {
+      return true;
+    } catch (IOException ignored) {
+      return false;
+    }
   }
 
-  public RabbitMQCollective(
-      String queueName,
-      String rabbitmqHost,
-      boolean workerNode,
-      String replyQueueName,
-      int queue_length) {
-    this.workerNode = workerNode;
-    this.rabbitmqHost = rabbitmqHost;
-    this.queueName = queueName;
-    this.replyQueue = replyQueueName;
-    this.queue_length = queue_length;
-    logger.info("rabbitmqHost: {}", rabbitmqHost);
-    logger.info("workerNode: {}", workerNode);
-    logger.info("ACTOR_LIMIT: {}", queue_length);
-  }
-
-  protected void runWorker(Channel channel) throws IOException, TimeoutException {
+  protected void runWorker(Channel channel) throws IOException {
     DeliverCallback deliverCallback =
         (consumerTag, delivery) -> {
           try {
-            this.doWorkerJob(delivery);
-          } catch (Exception var8) {
-            logger.error("[Worker] job failed...", var8);
+            doWorkerJob(delivery);
+          } catch (Exception e) {
+            logger.error("[Worker] job failed...", e);
           } finally {
             channel.basicPublish(
-                "",
-                delivery.getProperties().getReplyTo(),
-                (BasicProperties) null,
-                "Polo".getBytes());
+                "", delivery.getProperties().getReplyTo(), null, "Polo".getBytes());
+
             channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
             logger.info("[Worker] Send Ack");
           }
         };
-    channel.basicConsume(this.getQueueName(), false, deliverCallback, (consumerTag) -> {});
+    channel.basicConsume(getQueueName(), false, deliverCallback, consumerTag -> {});
   }
 
-  protected abstract void doWorkerJob(Delivery var1) throws IOException;
+  protected abstract void doWorkerJob(Delivery delivery) throws IOException;
 
   protected void runProducer(Channel channel) throws Exception {
-    this.setupResponseListener(channel);
-    BasicProperties props = (new Builder()).replyTo(this.getReplyQueue()).build();
-    this.doProducerJob(props);
+    setupResponseListener(channel);
+
+    AMQP.BasicProperties props =
+        new AMQP.BasicProperties.Builder().replyTo(getReplyQueue()).build();
+
+    doProducerJob(props);
   }
 
-  protected abstract void doProducerJob(BasicProperties var1) throws Exception;
+  protected abstract void doProducerJob(AMQP.BasicProperties props) throws Exception;
 
   private void setupResponseListener(Channel channel) throws IOException {
-    final BlockingQueue<String> response = new ArrayBlockingQueue(1);
+    final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
+
     channel.basicConsume(
-        this.getReplyQueue(),
+        getReplyQueue(),
         true,
         new DefaultConsumer(channel) {
+          @Override
           public void handleDelivery(
-              String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
-              throws UnsupportedEncodingException {
-            RabbitMQCollective.logger.info("[Producer] Received Ack");
+              String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
+            logger.info("[Producer] Received Ack");
             response.offer(new String(body, StandardCharsets.UTF_8));
-            RabbitMQCollective.this.actor_queue.poll();
-            RabbitMQCollective.logger.info("[Producer] Removed Element from Queue");
+            actor_queue.poll();
+            logger.info("[Producer] Removed Element from Queue");
           }
         });
   }
 
   protected void run() throws Exception {
-    this.preFlightCheck();
-    this.activeChannel = this.createChannel();
-    if (!this.workerNode) {
-      this.actor_queue = new ArrayBlockingQueue(this.queue_length);
-      this.runProducer(this.activeChannel);
-      logger.error("[Producer] Producer finished");
+    logger.info("Check if rabbitmq is up and running");
+    boolean rabbitIsAvailable = false;
+    while (!rabbitIsAvailable) {
+      rabbitIsAvailable = reachable();
+      if (!rabbitIsAvailable) {
+        logger.info("rabbitmq is not available waiting for {} sec", 30);
+        Thread.sleep(1000 * 30);
+      }
+    }
+
+    logger.info("Execute pre-flight check");
+    preFlightCheck();
+    activeChannel = createChannel();
+    if (!workerNode) {
+      logger.info("[Producer] Run Producer");
+      actor_queue = new ArrayBlockingQueue<>(queue_length);
+      runProducer(activeChannel);
     } else {
-      this.runWorker(this.activeChannel);
-      logger.error("[Worker] Worker finished");
+      logger.info("[Worker] Run Worker");
+      runWorker(activeChannel);
+      // usually only consumer define a prefetch counts
     }
   }
 
-  protected abstract void preFlightCheck();
+  /**
+   * Is executed before the worker/producer starts its job. Can be used to set up some required
+   * resources.
+   */
+  protected abstract void preFlightCheck() throws IOException;
 
   private Channel createChannel() throws IOException, TimeoutException {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost(this.rabbitmqHost);
+    factory.setHost(rabbitmqHost);
+    factory.setAutomaticRecoveryEnabled(true);
+    // attempt recovery every 10 seconds
+    factory.setNetworkRecoveryInterval(10000);
+    factory.setUsername(rabbitmqUser);
+    factory.setPassword(rabbitmqPass);
+
+    // do not call in try block like above, otherwise the channel is closed after the loop
     Connection connection = factory.newConnection();
+    connection.addShutdownListener(
+        cause -> {
+          logger.info("Received Connection Shutdown signal with cause: " + cause.getMessage());
+          RabbitMQCollective.this.shutdown();
+          final boolean hardError = cause.isHardError();
+          int signal = -1;
+
+          System.exit(signal);
+        });
+
     Channel channel = connection.createChannel();
-    channel.queueDeclare(this.queueName, false, false, false, (Map) null);
-    channel.basicQos(1);
+
+    channel.addShutdownListener(
+        cause -> {
+          logger.info("Received Channel Shutdown signal with cause: " + cause.getMessage());
+          RabbitMQCollective.this.shutdown();
+          final boolean hardError = cause.isHardError();
+          int signal = -1;
+
+          System.exit(signal);
+        });
+
+    channel.queueDeclare(queueName, false, false, false, null);
+    channel.basicQos(PREFETCH_COUNT);
+
     return channel;
   }
 
   public String getReplyQueue() {
-    return this.replyQueue;
+    return replyQueue;
   }
 
   public String getQueueName() {
-    return this.queueName;
+    return queueName;
   }
 
-  public void enqueue(BasicProperties props, byte[] body) throws IOException, InterruptedException {
-    this.actor_queue.put(body);
-    this.activeChannel.basicPublish("", this.getQueueName(), props, body);
+  /**
+   * Enqueues the given body to the rabbit queue.
+   *
+   * @param props Rabbit props acquired in the producer job
+   * @param body Body of the message that should be enqueued
+   * @throws InterruptedException
+   * @throws IOException
+   */
+  public void enqueue(AMQP.BasicProperties props, byte[] body)
+      throws IOException, InterruptedException {
+    actor_queue.put(body);
+    activeChannel.basicPublish("", getQueueName(), props, body);
   }
 
   public boolean isWorkerNode() {
-    return this.workerNode;
+    return workerNode;
   }
+
+  protected abstract void shutdown();
 }
---
+++ thetis-project-pipeline/src/main/java/de/upb/upcy/base/updatesteps/MainBuildPipeline.java
@@ -0,0 +1,95 @@
+package de.upb.upcy.base.updatesteps;
+
+import de.upb.upcy.base.updatesteps.buildnrunpipeline.MultiProjectRunner;
+import de.upb.upcy.base.updatesteps.buildnrunpipeline.Runner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainBuildPipeline {
+  /** Download link of the projects is https://zenodo.org/record/4479015#.Yf6JwvXMJhF */
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainBuildPipeline.class);
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+
+    // create the command line parser
+    CommandLineParser parser = new DefaultParser();
+
+    // create the Options
+    Options options = new Options();
+    options.addOption(
+        Option.builder("mp")
+            .longOpt("multi-projects")
+            .desc("use SIZE-byte blocks")
+            .hasArg()
+            .build());
+    options.addOption(
+        Option.builder("sp")
+            .longOpt("singe-project")
+            .desc("use SIZE-byte blocks")
+            .hasArg()
+            .build());
+    options.addOption(
+        Option.builder("o")
+            .longOpt("output")
+            .desc("folder to store csv files")
+            .hasArg()
+            .required(true)
+            .build());
+
+    try {
+      // parse the command line arguments
+      CommandLine line = parser.parse(options, args);
+      Path csvOutputDir = null;
+      if (line.hasOption("output")) {
+        // print the value of block-size
+        csvOutputDir = Paths.get(line.getOptionValue("output"));
+        if (!Files.exists(csvOutputDir)) {
+          Files.createDirectories(csvOutputDir);
+        }
+      }
+      Objects.requireNonNull(csvOutputDir);
+
+      // validate that block-size has been set
+      if (line.hasOption("singe-project")) {
+        // print the value of block-size
+        final String optionValue = line.getOptionValue("singe-project");
+
+        final Path projectPom = Paths.get(optionValue).resolve("pom.xml");
+        handleProject(projectPom.getParent().getFileName().toString(), projectPom, csvOutputDir);
+      } else if (line.hasOption("multi-projects")) {
+        // print the value of block-size
+        final String optionValue = line.getOptionValue("multi-projects");
+        handleMultiProject(optionValue, csvOutputDir);
+      }
+    } catch (ParseException exp) {
+      LOGGER.error("Unexpected exception:" + exp.getMessage());
+      // automatically generate the help statement
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp("help", options);
+    }
+  }
+
+  private static void handleMultiProject(String projectsRootFolder, Path outputDir)
+      throws IOException, InterruptedException {
+
+    MultiProjectRunner multiProjectRunner = new MultiProjectRunner(projectsRootFolder, outputDir);
+    multiProjectRunner.run();
+  }
+
+  private static void handleProject(String projectName, Path projectPomFile, Path outputDir) {
+    Runner projectRun = new Runner(projectName, projectPomFile, outputDir);
+    projectRun.run();
+  }
+}
